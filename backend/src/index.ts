245115bda23fda4a11ad39212890539e585c60a7
SampathM <maheshwarasampath@gmail.com>
2024-10-15 00:18:09 +0530

    first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,20 @@
+import {WebSocketServer} from 'ws'
+import { GameManager } from './Gamemanager';
+
+const wss = new WebSocketServer({port: 8080});
+
+const gameManager  = new GameManager();
+
+
+
+
+
+
+wss.on('connection', function connection(ws){
+    gameManager.addUser(ws);
+    ws.on('error', console.error);
+
+    ws.on("disconnect", () => {
+        gameManager.removeUser(ws);
+    })
+});
